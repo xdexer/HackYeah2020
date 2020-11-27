8c378d6ed4ebbabaa770c052923c18f6ae31c985
HoopDead <polec0@wp.pl>
2020-11-27 22:14:31 +0100

    Timer functionality & first UI attempt

---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeCounter : MonoBehaviour
+{
+
+    public float timeCounter = 0;
+    private TextMeshPro textMeshPro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMeshPro = gameObject.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+        timeCounter += Time.deltaTime;
+
+        float seconds = Mathf.FloorToInt(timeCounter % 60);
+        float minutes = Mathf.FloorToInt(timeCounter / 60);
+
+        string timeToString = string.Format("{00:00}:{1:00}", minutes, seconds);
+
+        textMeshPro.SetText(timeToString);
+        
+    }
+}
