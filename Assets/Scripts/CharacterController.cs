9accfbfb7d4a96cbd5087a4ff1e49efda80697af
slvi <darkwilk@gmail.com>
2020-11-27 20:41:18 +0100

    Added CharacterController, Character and Ground prefabs

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed;
+
+    public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+
+    // Determines how long after stepping out of the ground player can jump
+    public float rememberGroundedFor;
+    float lastTimeGrounded;
+
+    // Check if player is standing on GameObject with "Ground" layer
+    private bool isGrounded;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+
+    // Layer to detect which GameObject is ground - set every ground to "Ground" layer
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rigidBody;
+    private Vector2 moveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        BetterJump();
+        CheckIfGrounded();
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * speed;
+        rigidBody.velocity = new Vector2(moveBy, rigidBody.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+    }
+
+    void BetterJump()
+    {
+        if (rigidBody.velocity.y < 0)
+        {
+            rigidBody.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rigidBody.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+
+        if (collider != null)
+            isGrounded = true;
+        else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false;
+        }
+    }
+}
