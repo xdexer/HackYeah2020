732783ee533da26977d38e0c90fb762798f93664
Kamil <kkowalczyk848@gmail.com>
2020-11-28 14:32:10 +0100

    added trampoline

---
+++ Assets/Scripts/TrampolineLaunch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineLaunch : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float launchForce;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Trampoline"))
+        {
+            rb.velocity = Vector2.up * launchForce;
+        }
+    }
+
+}
