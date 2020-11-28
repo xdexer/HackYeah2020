574632f9ae25931a3f879f16c75acd944ef470d4
Kamil <kkowalczyk848@gmail.com>
2020-11-28 12:13:13 +0100

    added pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pauseObj;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseObj.SetActive(!pauseObj.activeInHierarchy);
+        if (pauseObj.activeInHierarchy == true)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void ResumeButton()
+    {
+        PauseGame();
+    }
+
+    public void MenuExitButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+}
