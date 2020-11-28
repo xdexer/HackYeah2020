9ce2bbe9abb6497874d92a361189e2cd8ba014db
HoopDead <polec0@wp.pl>
2020-11-28 23:24:36 +0100

    Created moving background in game

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject camera;
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() 
+    {
+        float temp = (camera.transform.position.y * (1 - parallaxEffect));
+        float dist = (camera.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(transform.position.x, startpos + dist, transform.position.z);
+
+        if(temp > startpos + length)
+        {
+            startpos += length;
+        } else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
+    }
+}
--- Assets/Scripts/BulletGenerator.cs
+++ Assets/Scripts/BulletGenerator.cs
@@ -9,7 +9,7 @@ public class BulletGenerator : MonoBehaviour
     private Vector2 spawnCoord;
     private Vector2 screenBounds;
     public float offset = 5.0f;
-    private bool spawner = true;
+    private bool spawner = false;
     private void Start()
     {
         Debug.Log(this.transform.position.x);
@@ -22,11 +22,15 @@ public class BulletGenerator : MonoBehaviour
     private void Update()
     {
         //Debug.Log(this.transform.position.y);
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-        //Debug.Log(screenBounds.y);
-        Vector3 GeneratorPosition = new Vector3(0.0f, screenBounds.y + offset);
-        this.transform.position = GeneratorPosition;
-        respawnTime = Random.Range(0.1f, 2.0f);
+        if(PointsCounter.pointsCounter < 20)
+        {
+            spawner = true;
+            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+            //Debug.Log(screenBounds.y);
+            Vector3 GeneratorPosition = new Vector3(0.0f, screenBounds.y + offset);
+            this.transform.position = GeneratorPosition;
+            respawnTime = Random.Range(0.1f, 2.0f);
+        }
     }
 
     private void spawnBullet()
