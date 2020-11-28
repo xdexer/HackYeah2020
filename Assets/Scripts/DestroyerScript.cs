75be21d4c4d31b428a2a19b34773879858bfe3e5
xdexer <marcinkowski.dominik@o2.pl>
2020-11-28 14:33:41 +0100

    added camera dependencies for generator

--- Assets/Scripts/DestroyerScript.cs
+++ Assets/Scripts/DestroyerScript.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class DestroyerScript : MonoBehaviour
 {
+    private Vector2 spawnCoord;
+    private Vector2 screenBounds;
+    public float offset = 5.0f;
+    private bool spawner = true;
+    private void Start()
+    {
+        Debug.Log(this.transform.position.x);
+        spawnCoord = this.transform.position;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+    }
+
+    private void Update()
+    {
+        Debug.Log(this.transform.position.y);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        Debug.Log(screenBounds.y);
+        Vector3 DestroyerPosition = new Vector3(0.0f, -screenBounds.y - offset);
+        this.transform.position = DestroyerPosition;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
             Debug.Log("Collision" + collision.gameObject.tag);
